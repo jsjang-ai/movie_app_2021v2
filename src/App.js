d793a49f65a734a0f327e2886feb808fc7de023d
jsjang-ai <jsjang.ai@gmail.com>
2021-07-21 23:29:41 +0900

    06-3 Movie 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function Food({ love }) {
-  return <h1>I like {love}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>hello!!!</h1>
-      <Food love="ramen" />
-      <Food love="samgiopsal" />
-      <Food love="jjajangmien" />
-      <Food love="chicken" />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>{isLoading
+        ? 'Loading...'
+        : movies.map((movie) => {
+          return (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+            />
+          );
+        })}</div>
+    );
+  }
 }
 
 export default App;
